d0fc920cd185f59a1fe7c8dcf6e396d431387bf6
Ayush khaskalam <ayushkhaskalam98@gmail.com>
2020-08-20 18:26:02 +0530

    About

--- Assests/js/main.js
+++ Assests/js/main.js
@@ -11,3 +11,21 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu('nav-toggle','nav-menu')
+
+
+
+//Active and remove menu
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    //Active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+//Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
