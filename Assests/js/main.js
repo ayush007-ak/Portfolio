ddf9675fba332a8abd94bbb3ee01ad6ef40c8d77
Ayush khaskalam <ayushkhaskalam98@gmail.com>
2020-08-29 21:18:58 +0530

    Complete

--- Assests/js/main.js
+++ Assests/js/main.js
@@ -29,3 +29,45 @@ function linkAction(){
 
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+//scroll reveal animation
+const sr  = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/*HOME */
+
+sr.reveal('.home_title',{})
+sr.reveal('.button',{delay:200})
+sr.reveal('.home_image',{delay: 400})
+sr.reveal('.home_social-icon',{interval:200})
+
+/* SCROLL ABOUT*/
+
+sr.reveal('.about_img',{})
+
+sr.reveal('.about_subtitle',{delay: 200})
+
+sr.reveal('.about_text',{delay: 400})
+
+/*skills scroll */
+
+sr.reveal('.skills_subtitle',{})
+
+sr.reveal('.skills_text',{delay: 200})
+
+sr.reveal('.skills_data',{interval: 200})
+
+sr.reveal('.skills_img',{delay: 400})
+
+/*work scroll */
+
+sr.reveal('.work_img',{interval:200})
+
+/*contact scroll */
+
+sr.reveal('.contact_input',{interval: 200})
